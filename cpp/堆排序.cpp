551a7f1bf2f2dd9ea5a7cf56249496f7bf9010eb
assumeengagetry <xsk13969662263@outlook.com>
2025-12-22 17:41:02 +0800

    feature: 增加堆排序

---
+++ cpp/堆排序.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+void max_heapify(int arr[], int start, int end) {
+    int dad = start;
+    int son = dad * 2 + 1;
+    while( son + 1 <= end){
+    if(arr[son] < arr[son+1] ){
+    son ++;}
+    if(arr[dad] > arr[son]){
+        return;
+    }
+    if(arr[dad]<arr[son]){
+        swap(arr[son],arr[dad]);
+        swap(son,dad);
+
+    }
+    }
+}
+
+void heap_sort(int arr[], int len) {
+    for (int i = len / 2 - 1; i >= 0; i--)
+        max_heapify(arr, i, len - 1);
+
+    for (int i = len - 1; i > 0; i--) {
+        swap(arr[0], arr[i]);
+        max_heapify(arr, 0, i - 1);
+    }
+}
+
+int main() {
+    int arr[] = { 3, 5, 3, 0, 8, 6, 1, 5, 8, 6, 2, 4, 9, 4, 7, 0, 1, 8, 9, 7, 3, 1, 2, 5, 9, 7, 4, 0, 2, 6 };
+    int len = (int) sizeof(arr) / sizeof(*arr);
+    heap_sort(arr, len);
+    for (int i = 0; i < len; i++)
+        cout << arr[i] << ' ';
+    cout << endl;
+    return 0;
+}
